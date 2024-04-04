65fa7cbdb323c23f031281773d5de648c1879d36
Makar Sidorov <makar.sidorov@idpllc.ru>
2024-04-04 19:03:45 +0300

    add rabbit tets

--- main.py
+++ main.py
@@ -1,29 +1,54 @@
 import numpy as np
-
 from matplotlib import pyplot as plt
+from plyfile import PlyData
 
 from Point import Point
 from PointCloud import PointCloud
 from ICP import ICP
 
+
+def read_ply_file(file_path):
+    plydata = PlyData.read(file_path)
+
+    vertices = plydata['vertex']
+
+    x = vertices['x']
+    y = vertices['y']
+    z = vertices['z']
+
+    points = np.vstack((x, y, z)).T
+
+    return points
+
+
 if __name__ == '__main__':
+    A = read_ply_file('tests/bun000.ply')
+    B = read_ply_file('tests/bun045.ply')
+
+    fig = plt.figure(figsize=(15, 15))
     ax = plt.axes(projection='3d')
-    ax.set_xlim(-2, 2)
-    ax.set_ylim(-2, 2)
-    ax.set_zlim(-2, 2)
+    ax.set_xlabel('X (m)')
+    ax.set_ylabel('Y (m)')
+    ax.set_zlabel('Z (m)')
 
-    target = PointCloud(
-        [Point(-2, -4, 0), Point(-1, -2, 0), Point(0, 0, 0), Point(1, 2, 0), Point(2, 4, 0), Point(1, 1, 0),
-         Point(-1, -1, 0)])
+    source = PointCloud([Point(p[0], p[1], p[2]) for p in A.tolist()[:40000]])
+    target = PointCloud([Point(p[0], p[1], p[2]) for p in B.tolist()[:40000]])
 
-    source_array = np.random.permutation(target.get_matrix())
-    source = PointCloud([Point(p[0], p[1], p[2]) for p in source_array]).do_perturbation()
+    ax = plt.axes(projection='3d')
 
-    ICP(source, target).draw_steps(ax, 10, 1)
-
-    # target.draw(ax, 'g')
-    # source.draw(ax, 'k')
+    target.draw(ax, 'g')
+    source.draw(ax, 'k')
     plt.show()
+
+
+    # target = PointCloud(
+    #     [Point(-2, -4, 0), Point(-1, -2, 0), Point(0, 0, 0), Point(1, 2, 0), Point(2, 4, 0), Point(1, 1, 0),
+    #      Point(-1, -1, 0)])
+    #
+    # source_array = np.random.permutation(target.get_matrix())
+    # source = PointCloud([Point(p[0], p[1], p[2]) for p in source_array]).do_perturbation()
+    #
+    # ICP(source, target).draw_steps(ax, 10, 1)
 
 # tests
 # [Point(-2, -4, 0), Point(-1, -2, 0), Point(0, 0, 0), Point(1, 2, 0), Point(2, 4, 0)]

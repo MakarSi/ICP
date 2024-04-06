a7e6f77e2f50ea23e1f29453053bc327cbd2873b
Makar Sidorov <makar.sidorov@idpllc.ru>
2024-04-06 21:35:31 +0300

    fix

--- src/KDTree.py
+++ src/KDTree.py
@@ -1,6 +1,4 @@
 import itertools
-import random
-
 from recordtype import recordtype
 from typing import List, Tuple
 from Metric import Metric, MetricContainer
@@ -17,6 +15,10 @@ class BoundaryBox:
         self._dimension = cls.dimension()
         self._bounds = [MinMax(inf, -inf) for _ in range(self._dimension)]
         self._cls = cls
+
+    @property
+    def dimension(self):
+        return self._dimension
 
     @property
     def bounds(self):
@@ -68,36 +70,33 @@ class KDTree:
         self.tree = [BoundaryBox(self._cls, self._inf)] * 2
         self.l_tr, self.r_tr = [-1] * 2, [-1] * 2
 
-    def build(self, point_cloud: MetricContainer, tr_num=1, turn=0):
+    def _add_node(self):
+        self.tree.append(BoundaryBox(self._cls, self._inf))
+        self.l_tr.append(-1)
+        self.r_tr.append(-1)
+
+    def build(self, point_cloud: MetricContainer, tr_num=1, coordinate=0):
         if point_cloud.metric_type() != self._cls:
             raise AttributeError(f"Container have to contain {self._cls} objects")
 
         for point in point_cloud:
             self.tree[tr_num].update(point)
-
-        if len(point_cloud) == 1:
+        if len(point_cloud) <= 1:
             return
 
-        point_cloud.sort(key=lambda p: (p[turn]))
+        point_cloud.sort(key=lambda p: (p[coordinate]))
 
-        mid, left_points, right_points = len(point_cloud) // 2, [], []
-        for i in range(mid):
-            left_points.append(point_cloud[i])
-        for i in range(mid, len(point_cloud)):
-            right_points.append(point_cloud[i])
+        # Initialize left sub-tree
+        self._add_node()
+        self.l_tr[tr_num] = len(self.tree) - 1
+        self.build(self._container_cls(point_cloud[len(point_cloud) // 2:]), self.l_tr[tr_num],
+                   (coordinate + 1) % self._dimension)
 
-        if len(left_points) > 0:
-            self.tree.append(BoundaryBox(self._cls, self._inf))
-            self.l_tr.append(-1)
-            self.r_tr.append(-1)
-            self.l_tr[tr_num] = len(self.tree) - 1
-            self.build(self._container_cls(left_points), self.l_tr[tr_num], (turn + 1) % self._dimension)
-        if len(right_points) > 0:
-            self.tree.append(BoundaryBox(self._cls, self._inf))
-            self.l_tr.append(-1)
-            self.r_tr.append(-1)
-            self.r_tr[tr_num] = len(self.tree) - 1
-            self.build(self._container_cls(right_points), self.r_tr[tr_num], (turn + 1) % self._dimension)
+        # Initialize right sub-tree
+        self._add_node()
+        self.r_tr[tr_num] = len(self.tree) - 1
+        self.build(self._container_cls(point_cloud[:len(point_cloud) // 2]), self.r_tr[tr_num],
+                   (coordinate + 1) % self._dimension)
 
     def find_closest(self, point: Metric) -> Tuple[float, Metric]:
         """
@@ -109,31 +108,21 @@ class KDTree:
         Returns:
              Tuple[float, Point] - (расстояние до ближайшей точки, ближайшая точка).
         """
-        # TODO: избавиться от говнокода
+
+        def _find_closest(tr_num=1):
+            nonlocal min_dist, res
+            if self.l_tr[tr_num] == -1:
+                min_dist = self.tree[tr_num].distance(point)
+                res = self._cls(*[self.tree[tr_num].bounds[i].min for i in range(self._dimension)])
+                return
+            if self.tree[self.l_tr[tr_num]].distance(point) < min_dist:
+                _find_closest(self.l_tr[tr_num])
+            if self.tree[self.r_tr[tr_num]].distance(point) < min_dist:
+                _find_closest(self.r_tr[tr_num])
+
         if type(point) != self._cls:
             raise AttributeError(f"Point have to be {self._cls}")
-        min_dist = [self._inf, 0]
-        res = [self._cls()]
-        self._find_closest(point, min_dist, res)
-        return min_dist[0], res[0]
-
-    def _find_closest(self, point, min_dist=None, res=None, tr_num=1):
-        if type(point) != self._cls:
-            raise AttributeError(f"Point have to be {self._cls}")
-
-        min_dist[1] += 1
-        if self.l_tr[tr_num] == -1:
-            min_dist[0] = self.tree[tr_num].distance(point)
-            res[0] = self._cls(*[self.tree[tr_num].bounds[i].min for i in range(self._dimension)])
-            return
-        if random.randint(0, 1):
-            if self.tree[self.l_tr[tr_num]].distance(point) < min_dist[0]:
-                self._find_closest(point, min_dist, res, self.l_tr[tr_num])
-            if self.tree[self.r_tr[tr_num]].distance(point) < min_dist[0]:
-                self._find_closest(point, min_dist, res, self.r_tr[tr_num])
-        else:
-            if self.tree[self.r_tr[tr_num]].distance(point) < min_dist[0]:
-                self._find_closest(point, min_dist, res, self.r_tr[tr_num])
-            if self.tree[self.l_tr[tr_num]].distance(point) < min_dist[0]:
-                self._find_closest(point, min_dist, res, self.l_tr[tr_num])
-        return min_dist[0], res
+        min_dist = self._inf
+        res = self._cls()
+        _find_closest()
+        return min_dist, res
